f7a43ad5493bcab44f0aa2718edfcf195acf811f
Blackbottt <ryanchipir@gmail.com>
2026-09-15 09:45:09 +0200

    toggle_task toggles a tasks completion

--- logic.py
+++ logic.py
@@ -6,6 +6,10 @@ def add_task(title):
     tasks.append({'id': len(tasks) + 1, 'title': title, 'completed': False})
     return tasks
 def toggle_task(id):
-    pass
+    for task in tasks:
+        if task['id'] == id:
+            task['completed'] = not task['completed']
+            return task
+    return None
 def delete_task(id):
     pass
