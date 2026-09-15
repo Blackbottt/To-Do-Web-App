387e2e3a2ec7f15327c74dd1285ac239aae3e9a4
Blackbottt <ryanchipir@gmail.com>
2026-09-15 11:17:24 +0200

    created a POST Method create_task_route

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 import logic
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hallo Blackbottt, your backend is alive"
+
+@app.route("/tasks", methods=["POST"])
+def create_task_route():
+    data = request.get_json()
+    title = data.get("title")
+    if not title:
+        return {"error": "Title is required"}, 400
+    logic.add_task(title)
+    return {"message": "Task added successfully"}, 201
 
 @app.route("/tasks")
 def get_tasks_route():
