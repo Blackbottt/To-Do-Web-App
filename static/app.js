1da45dcd942687f8c86eb520180970805ba9a922
Blackbottt <ryanchipir@gmail.com>
2026-09-16 13:38:45 +0200

    gave the completed tasks message its own element

--- static/app.js
+++ static/app.js
@@ -6,10 +6,11 @@ const tasksCompleted = function(tasks) {
     if (tasks) {
         if (tasks.every(task => task.completed)) {
             const allCompletedMessage = document.createElement('div');
+            const message = document.createElement('p');
             const buttons = document.createElement('div');
             const deleteButton = document.createElement('button');
             const storeButton = document.createElement('button');
-            allCompletedMessage.textContent = 'All tasks completed!\n Would you like to store the List or Delete it?';
+            message.textContent = 'All tasks completed!\n Would you like to store the List or Delete it?';
             allCompletedMessage.classList.add('all-completed-message');
             deleteButton.textContent = 'Delete List';   
             storeButton.textContent = 'Store List'; 
