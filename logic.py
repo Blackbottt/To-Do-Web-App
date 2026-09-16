4cfc1017af7c7380caa554a7a437b5532aa82e2b
Blackbottt <ryanchipir@gmail.com>
2026-09-16 14:25:45 +0200

    added in logic delete all tasks function

--- logic.py
+++ logic.py
@@ -2,6 +2,7 @@ tasks = [{'task_id': 1, 'title': 'Task 1', 'completed': False}]
 
 def get_tasks():
     return tasks
+
 def add_task(title):
     if not tasks:
         task_id = 1
@@ -9,15 +10,21 @@ def add_task(title):
         task_id = max(task['task_id'] for task in tasks) + 1
     tasks.append({'task_id': task_id, 'title': title, 'completed': False})
     return tasks
+
 def toggle_task(task_id):
     for task in tasks:
         if task['task_id'] == task_id:
             task['completed'] = not task['completed']
             return task
     return None
+
 def delete_task(task_id):
     for task in tasks:
         if task['task_id'] == task_id:
             tasks.remove(task)
             return task
     return None
+
+def delete_all_tasks():
+    tasks.clear()
+    return tasks
