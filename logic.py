6d1080648bd2b25f8a37fdc530fe6e199c004fbe
Blackbottt <ryanchipir@gmail.com>
2026-09-17 08:33:22 +0200

    added save tasks to file function to all feature functions below

--- logic.py
+++ logic.py
@@ -5,15 +5,12 @@ import os
 
 def load_tasks_from_file():
     global tasks, list_of_tasks
-
     if not os.path.exists('tasks.json'):
         tasks = []
         list_of_tasks = []
         return
-    
     with open('tasks.json', 'r') as f:
         data = json.load(f)
-        
     tasks = data.get('tasks', [])
     list_of_tasks = data.get('list_of_tasks', [])
 
@@ -37,6 +34,7 @@ def add_task(title):
     else:
         task_id = max(task['task_id'] for task in tasks) + 1
     tasks.append({'task_id': task_id, 'title': title, 'completed': False})
+    save_tasks_to_file()
     return tasks
 
 def add_task_list(title):
@@ -45,12 +43,14 @@ def add_task_list(title):
     else:
         list_id = max(task_list['list_id'] for task_list in list_of_tasks) + 1
     list_of_tasks.append({'list_id': list_id, 'title': title, 'tasks': []})
+    save_tasks_to_file()
     return list_of_tasks
 
 def toggle_task(task_id):
     for task in tasks:
         if task['task_id'] == task_id:
             task['completed'] = not task['completed']
+            save_tasks_to_file()
             return task
     return None
 
@@ -58,9 +58,11 @@ def delete_task(task_id):
     for task in tasks:
         if task['task_id'] == task_id:
             tasks.remove(task)
+            save_tasks_to_file()
             return task
     return None
 
 def delete_all_tasks():
     tasks.clear()
+    save_tasks_to_file()
     return tasks
